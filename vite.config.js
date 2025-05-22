57c7f079852ebb95e3bba2a71aef1f4e7f511886
dachy <dachmachu@gmail.com>
2025-05-22 21:37:22 +0400

    Fix: Update vite config to use default minifier instead of terser

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5174,
+  build: {
+    // Using the default minifier (esbuild) instead of terser
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
   },
 })
